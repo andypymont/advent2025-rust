6d39017ba8a686155309095b1b7e264a035915f8
Andy Pymont <andy@andypymont.co.uk>
2025-12-11 10:50:55 +0000

    Day 10 Part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,3 +1,4 @@
+use microlp::{ComparisonOp, LinearExpr, OptimizationDirection, Problem, Variable};
 use std::collections::BTreeSet;
 use std::str::FromStr;
 
@@ -5,20 +6,19 @@ advent_of_code::solution!(10);
 
 #[derive(Debug, PartialEq)]
 struct Machine {
-    indicators: u64,
     pattern: u64,
     buttons: Vec<u64>,
-    requirements: Vec<u64>,
+    joltages: Vec<u64>,
 }
 
 impl Machine {
-    fn fewest_button_presses(&self) -> u64 {
+    fn fewest_button_presses(&self) -> Option<u64> {
         let mut queue = BTreeSet::new();
         queue.insert((0, 0));
 
         while let Some((presses, lights)) = queue.pop_first() {
             if lights == self.pattern {
-                return presses;
+                return Some(presses);
             }
 
             queue.extend(
@@ -28,7 +28,37 @@ impl Machine {
             );
         }
 
-        u64::MAX
+        None
+    }
+
+    #[allow(clippy::cast_possible_truncation)]
+    #[allow(clippy::cast_precision_loss)]
+    #[allow(clippy::cast_sign_loss)]
+    fn fewest_button_presses_joltage(&self) -> Option<u64> {
+        let mut problem = Problem::new(OptimizationDirection::Minimize);
+
+        let vars: Vec<Variable> = self
+            .buttons
+            .iter()
+            .map(|_| problem.add_integer_var(1.0, (0, i32::MAX)))
+            .collect();
+
+        for (constraint, joltage) in self.joltages.iter().enumerate() {
+            let mut equation = LinearExpr::empty();
+
+            for (variable, button) in self.buttons.iter().enumerate() {
+                if button & (1 << constraint) != 0 {
+                    equation.add(vars[variable], 1.0);
+                }
+            }
+
+            problem.add_constraint(equation, ComparisonOp::Eq, *joltage as f64);
+        }
+
+        problem
+            .solve()
+            .ok()
+            .map(|solution| solution.objective().round() as u64)
     }
 }
 
@@ -41,7 +71,14 @@ impl Factory {
     fn fewest_button_presses(&self) -> u64 {
         self.machines
             .iter()
-            .map(Machine::fewest_button_presses)
+            .filter_map(Machine::fewest_button_presses)
+            .sum()
+    }
+
+    fn fewest_button_presses_joltage(&self) -> u64 {
+        self.machines
+            .iter()
+            .filter_map(Machine::fewest_button_presses_joltage)
             .sum()
     }
 }
@@ -54,22 +91,18 @@ impl FromStr for Machine {
 
     fn from_str(line: &str) -> Result<Self, Self::Err> {
         let (indicators, line) = line.split_once(' ').ok_or(ParseFactoryError)?;
-        let (buttons, requirements) = line.rsplit_once(' ').ok_or(ParseFactoryError)?;
-
-        let (indicators, pattern) = indicators
-            .chars()
-            .filter_map(|ch| match ch {
-                '.' => Some(0),
-                '#' => Some(1),
-                _ => None,
-            })
-            .fold((0, 0), |(indicators, pattern), on| {
-                (indicators + 1, pattern << 1 | on)
-            });
+        let (buttons, joltages) = line.rsplit_once(' ').ok_or(ParseFactoryError)?;
+        let indicators = indicators
+            .strip_prefix("[")
+            .and_then(|ind| ind.strip_suffix("]"))
+            .ok_or(ParseFactoryError)?;
 
         Ok(Self {
-            indicators,
-            pattern,
+            pattern: indicators
+                .chars()
+                .rev()
+                .map(|ch| u64::from(ch == '#'))
+                .fold(0, |pattern, on| pattern << 1 | on),
             buttons: buttons
                 .split_whitespace()
                 .filter_map(|button| {
@@ -80,13 +113,11 @@ impl FromStr for Machine {
                             button
                                 .split(',')
                                 .filter_map(|light| light.parse().ok())
-                                .fold(0, |lights, light: u64| {
-                                    lights + (1 << (indicators - light - 1))
-                                })
+                                .fold(0, |lights, light: u64| lights | (1 << light))
                         })
                 })
                 .collect(),
-            requirements: requirements
+            joltages: joltages
                 .strip_prefix("{")
                 .and_then(|reqs| reqs.strip_suffix("}"))
                 .unwrap_or("")
@@ -118,10 +149,12 @@ pub fn part_one(input: &str) -> Option<u64> {
         .map(Factory::fewest_button_presses)
 }
 
-#[allow(clippy::missing_const_for_fn)]
 #[must_use]
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    Factory::from_str(input)
+        .ok()
+        .as_ref()
+        .map(Factory::fewest_button_presses_joltage)
 }
 
 #[cfg(test)]
@@ -132,22 +165,19 @@ mod tests {
         Factory {
             machines: vec![
                 Machine {
-                    indicators: 4,
                     pattern: 6,
-                    buttons: vec![1, 5, 2, 3, 10, 12],
-                    requirements: vec![3, 5, 4, 7],
+                    buttons: vec![8, 10, 4, 12, 5, 3],
+                    joltages: vec![3, 5, 4, 7],
                 },
                 Machine {
-                    indicators: 5,
-                    pattern: 2,
-                    buttons: vec![23, 6, 17, 28, 15],
-                    requirements: vec![7, 5, 12, 7, 2],
+                    pattern: 8,
+                    buttons: vec![29, 12, 17, 7, 30],
+                    joltages: vec![7, 5, 12, 7, 2],
                 },
                 Machine {
-                    indicators: 6,
-                    pattern: 29,
-                    buttons: vec![62, 38, 59, 24],
-                    requirements: vec![10, 11, 11, 5, 10, 5],
+                    pattern: 46,
+                    buttons: vec![31, 25, 55, 6],
+                    joltages: vec![10, 11, 11, 5, 10, 5],
                 },
             ],
         }
@@ -164,9 +194,9 @@ mod tests {
     #[test]
     fn test_fewest_button_presses() {
         let factory = example_factory();
-        assert_eq!(factory.machines[0].fewest_button_presses(), 2);
-        assert_eq!(factory.machines[1].fewest_button_presses(), 3);
-        assert_eq!(factory.machines[2].fewest_button_presses(), 2);
+        assert_eq!(factory.machines[0].fewest_button_presses(), Some(2));
+        assert_eq!(factory.machines[1].fewest_button_presses(), Some(3));
+        assert_eq!(factory.machines[2].fewest_button_presses(), Some(2));
     }
 
     #[test]
@@ -176,8 +206,25 @@ mod tests {
     }
 
     #[test]
+    fn test_fewest_button_presses_joltage() {
+        let factory = example_factory();
+        assert_eq!(
+            factory.machines[0].fewest_button_presses_joltage(),
+            Some(10)
+        );
+        assert_eq!(
+            factory.machines[1].fewest_button_presses_joltage(),
+            Some(12)
+        );
+        assert_eq!(
+            factory.machines[2].fewest_button_presses_joltage(),
+            Some(11)
+        );
+    }
+
+    #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(33));
     }
 }
